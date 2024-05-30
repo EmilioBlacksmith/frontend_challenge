a5119ae07015b21511509c42aa39e78ee32199b9
emilioblacksmith <emilioblacksmithlush@gmail.com>
2024-05-30 00:04:38 -0600

    Initial tailwind setup & add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		colors: {
+			black: "#000000",
+			white: "#FFFFFF",
+			dark_gray: "#111111",
+			gray: "#7f7f7f",
+			light_gray: "#cccccc",
+			main_color: "#40c1ad",
+		},
+		fontFamily: {
+			sans: ["Agave"],
+		},
 		extend: {},
 	},
 	plugins: [],

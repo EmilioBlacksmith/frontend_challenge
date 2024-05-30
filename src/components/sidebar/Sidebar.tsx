dca04cfd6181134fe5872d33bd891210b98d1542
emilioblacksmith <emilioblacksmithlush@gmail.com>
2024-05-30 01:19:37 -0600

    add sidebar button states

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import SidebarButton from "./SidebarButton";
 
 const Sidebar: React.FC = () => {
-	const handleClick = () => {
+	const [selectedButton, setSelectedButton] = useState<number | null>(0);
+
+	const handleClick = (buttonIndex: number) => {
+		setSelectedButton(buttonIndex);
 		console.log("Button Clicked");
 	};
 
 	return (
-		<nav className="h-full w-20 outline outline-1 outline-gray">
+		<nav className="h-full w-20 outline outline-1 outline-gray flex flex-col justify-evenly">
 			<SidebarButton
-				onClick={handleClick}
+				onClick={() => handleClick(0)}
 				label=""
+				selected={selectedButton === 0}
+			/>
+			<SidebarButton
+				onClick={() => handleClick(1)}
+				label="󰿎"
+				selected={selectedButton === 1}
+			/>
+			<SidebarButton
+				onClick={() => handleClick(2)}
+				label="󰟴"
+				selected={selectedButton === 2}
+			/>
+			<SidebarButton
+				onClick={() => handleClick(3)}
+				label=""
+				selected={selectedButton === 3}
 			/>
 		</nav>
 	);
--- src/components/sidebar/SidebarButton.tsx
+++ src/components/sidebar/SidebarButton.tsx
@@ -3,12 +3,13 @@ import React from "react";
 interface SidebarButtonProps {
 	onClick: () => void;
 	label: string;
+    selected: boolean;
 }
 
-const SidebarButton: React.FC<SidebarButtonProps> = ({ onClick, label }) => {
+const SidebarButton: React.FC<SidebarButtonProps> = ({ onClick, label, selected }) => {
 	return (
 		<button
-			className="w-full h-8 text-main_color text-3xl"
+			className={`w-full h-8 text-3xl ${selected ? 'text-main_color' : 'text-white'}`}
 			onClick={onClick}
 		>
 			{label}

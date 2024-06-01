2a43a3cb5a0929f9f5c09b6559c70668f2972d47
emilioblacksmith <emilioblacksmithlush@gmail.com>
2024-06-01 05:53:29 -0600

    add detail section fetching feature. TODO: - [ ] Enhance graphics - [ ] Edit Changes based if movie or TV

--- src/views/Detail.tsx
+++ src/views/Detail.tsx
@@ -1,13 +1,76 @@
 import { useSection } from "../contexts/SectionContext";
+import { useEffect, useState } from "react";
+
+const posterURL: string = "https://image.tmdb.org/t/p/w500/";
+const backdropURL: string = "https://image.tmdb.org/t/p/original/";
+
+interface MediaData {
+	id: number;
+	title: string;
+	poster_path: string;
+	vote_average: number;
+	release_date: string;
+	runtime: number;
+	backdrop_path: string;
+	overview: string;
+	vote_count: number;
+}
 
 const Detail = () => {
 	const { currentSection, selectedMediaId, selectedMediaType } = useSection();
+	const [mediaData, setMediaData] = useState<MediaData | null>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<Error | null>(null);
+
+	useEffect(() => {
+		if (selectedMediaId === null) return;
+
+		const options = {
+			method: "GET",
+			headers: {
+				accept: "application/json",
+				Authorization:
+					"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMWVlNWI5YTEwOTg1NDhmZmQxZTUwZDA5ZWM4ZmM4NCIsInN1YiI6IjY2NTI3YWZmMDJmMjk0Zjc1MTI2ZmQ0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6nGhr7cnoGYhyXq8QP2wup3cETm2J84ThjhAZPJX7Zc",
+			},
+		};
+
+		setLoading(true);
+
+		fetch(
+			`https://api.themoviedb.org/3/${selectedMediaType}/${selectedMediaId}?language=en-US`,
+			options
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				setMediaData(data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				setError(err);
+				setLoading(false);
+			});
+	}, [selectedMediaId, selectedMediaType]);
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	}
 
 	return (
 		<>
-			{currentSection === "detail" && (
+			{currentSection === "detail" && mediaData && (
 				<div className="w-full h-full p-1 bg-dark_gray">
-					This is the detail for... {selectedMediaId} {selectedMediaType}
+					<div key={mediaData.id}>
+						<h1>This is the info for: {mediaData.title}</h1>
+						<p>{mediaData.overview}</p>
+						<img
+							src={`${posterURL}${mediaData.poster_path}`}
+							alt={mediaData.title}
+						/>
+					</div>
 				</div>
 			)}
 		</>
